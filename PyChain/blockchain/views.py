d83946894e7acea488eca65318e5c06ef8668229
Vladimir <bigcorvet@gmail.com>
2022-12-30 14:04:45 +0200

    add def is_valid, get_chain, mine_block

--- PyChain/blockchain/views.py
+++ PyChain/blockchain/views.py
@@ -9,13 +9,13 @@ class Blockchain:
 
     def __init__(self) -> None:
         self.chain = []
-        self.create_block(nonce=1, previous_hash="0")
+        self.create_block(proof=1, previous_hash="0")
 
-    def create_block(self, nonce, previous_hash):
+    def create_block(self, proof, previous_hash):
         block = {
             "index": len(self.chain) + 1,
             "timestamp": str(datetime.datetime.now()),
-            "nonce": nonce,
+            "proof": proof,
             "previous_hash": previous_hash,
         }
         self.chain.append(block)
@@ -24,17 +24,18 @@ class Blockchain:
     def get_previous_block(self):
         return self.chain[-1]
 
-    def proof_of_work(self, previous_nonce):
-        new_nonce = 1
-        check_nonce = False
+    def proof_of_work(self, previous_proof):
+        new_proof = 1
+        check_proof = False
         
-        while check_nonce is False:
-            hash_operation = hashlib.sha256(str(new_nonce**2 - previous_nonce**2).encode()).hexdigest()
+        while check_proof is False:
+            hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
+
             if hash_operation[:4] == '0000':
-                check_nonce = True
+                check_proof = True
             else:
-                new_nonce += 1
-        return new_nonce
+                new_proof += 1
+        return new_proof
     
 
     def hash(self, block):
@@ -49,9 +50,9 @@ class Blockchain:
             block = chain[block_index]
             if block['previous_hash'] != self.hash(previous_block):
                 return False
-            previous_nonce = previous_block['nonce']
-            nonce = block['nonce']
-            hash_operation = hashlib.sha256(str(nonce**2 - previous_nonce**2).encode()).hexdigest()
+            previous_proof = previous_block['proof']
+            proof = block['proof']
+            hash_operation = hashlib.sha256(str(proof**2 - previous_proof**2).encode()).hexdigest()
 
             if hash_operation[:4] != '0000':
                 return False
@@ -64,15 +65,35 @@ blockchain = Blockchain()
 def mine_block(request):
     if request.method == 'GET':
         previous_block = blockchain.get_previous_block()
-        previous_nonce = previous_block['nonce']
-        nonce = blockchain.proof_of_work(previous_nonce)
+        previous_proof = previous_block['proof']
+        proof = blockchain.proof_of_work(previous_proof)
         previous_hash = blockchain.hash(previous_block)
-        block = blockchain.create_block(nonce, previous_hash)
+        block = blockchain.create_block(proof, previous_hash)
         response = {
             'message': 'Поздравляем! Вы только что создали блок.',
             'index': block['index'],
             'timestamp': block['timestamp'],
-            'nonce': block['nonce'],
+            'proof': block['proof'],
             'previous_hash': block['previous_hash']
         }
+    return JsonResponse(response)
+
+def get_chain(request):
+    if request.method == 'GET':
+        response = {
+            'chain': blockchain.chain,
+            'length': len(blockchain.chain)
+        }
+    return JsonResponse(response)
+
+
+def is_valid(request):
+    if request.nethod == 'GET':
+        is_valid = blockchain.is_chain_valid(blockchain.chain)
+
+        if is_valid:
+            response = {'message': 'Все хорошо! Данные blockchaine валидны!'}
+        else:
+            response = {'message': 'Ошибка. Что-то пошло не так!!'}
+            
     return JsonResponse(response)
